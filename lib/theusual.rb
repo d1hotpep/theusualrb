27e0a518ffd2272e7f15240df59eb1af1d1539e6
dpepper <pepper.daniel@gmail.com>
2017-01-17 16:30:54 -1000

    improve loading system

--- lib/theusual.rb
+++ lib/theusual.rb
@@ -1,9 +1,22 @@
 require_relative 'theusual/array.rb'
-require_relative 'theusual/mongo.rb'
 require_relative 'theusual/hash.rb'
-require_relative 'theusual/net.rb'
 
 
 module TheUsual
   VERSION = '0.0.1'
+
+  def self.load *modules
+    paths = {
+      'ipaddr' => 'ipaddr',
+      'mongoid' => 'mongoid',
+      'net/ssh' => 'ssh',
+    }
+
+    modules.flatten.each do |_module|
+      raise ArgumentException unless paths.has_key? _module
+
+      require _module.to_s
+      require_relative "theusual/#{paths[_module.to_s]}.rb"
+    end
+  end
 end
