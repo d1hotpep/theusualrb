896ae947f8300760ae326b33f7f0cd37c069cde9
dpepper <pepper.daniel@gmail.com>
2017-01-17 17:22:09 -1000

    comments

--- lib/theusual.rb
+++ lib/theusual.rb
@@ -11,12 +11,14 @@ module TheUsual
   ]
 
   def self.load *modules
+    # some modules need to be explicitly required
     needs_load = [
       'ipaddr',
       'mongoid',
       'net/ssh',
     ]
 
+    # some of our filenames need to be remapped
     paths = {
       'net/ssh' => 'ssh',
     }
@@ -26,8 +28,10 @@ module TheUsual
     modules.flatten.map(&:to_s).each do |_module|
       raise ArgumentException unless MODULES.include? _module
 
+      # load standard lib
       require _module if needs_load.include? _module
 
+      # monkey patch
       name = paths[_module] || _module
       require_relative "theusual/#{name}.rb"
     end
