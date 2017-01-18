7f25db96b7c51a133ebcf453d0c4a0f6bbb66860
dpepper <pepper.daniel@gmail.com>
2017-01-17 15:50:27 -1000

    guard in case lib is not around

--- lib/theusual/net.rb
+++ lib/theusual/net.rb
@@ -1,49 +1,53 @@
 # Most of this code was copied from:
 # http://www.rubydoc.info/github/net-ssh/net-ssh/Net/SSH/Connection/Session
 
-class Net::SSH::Connection::Session
+if defined? Net::SSH
 
-    # use a psuedo TTY so we can run sudo commands on more secured cloud instances
-    def suexec(command, &block)
-      open_channel do |channel|
-        channel.request_pty
+  class Net::SSH::Connection::Session
 
-        # add sudo prefix if need be
-        command = "sudo #{command}" unless /^sudo /.match command
+      # use a psuedo TTY so we can run sudo commands on more secured cloud instances
+      def suexec(command, &block)
+        open_channel do |channel|
+          channel.request_pty
 
-        channel.exec(command) do |ch, success|
-          raise "could not execute command: #{command.inspect}" unless success
+          # add sudo prefix if need be
+          command = "sudo #{command}" unless /^sudo /.match command
 
-          channel.on_data do |ch2, data|
-            if block
-              block.call(ch2, :stdout, data)
-            else
-              $stdout.print(data)
+          channel.exec(command) do |ch, success|
+            raise "could not execute command: #{command.inspect}" unless success
+
+            channel.on_data do |ch2, data|
+              if block
+                block.call(ch2, :stdout, data)
+              else
+                $stdout.print(data)
+              end
             end
-          end
 
-          channel.on_extended_data do |ch2, type, data|
-            if block
-              block.call(ch2, :stderr, data)
-            else
-              $stderr.print(data)
+            channel.on_extended_data do |ch2, type, data|
+              if block
+                block.call(ch2, :stderr, data)
+              else
+                $stderr.print(data)
+              end
             end
           end
         end
       end
-    end
 
 
-    def suexec!(command, &block)
-      block ||= Proc.new do |ch, type, data|
-        ch[:result] ||= ""
-        ch[:result] << data
+      def suexec!(command, &block)
+        block ||= Proc.new do |ch, type, data|
+          ch[:result] ||= ""
+          ch[:result] << data
+        end
+
+        channel = suexec(command, &block)
+        channel.wait
+
+        return channel[:result]
       end
 
-      channel = suexec(command, &block)
-      channel.wait
-
-      return channel[:result]
-    end
+  end
 
 end
